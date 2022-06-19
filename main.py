b9f44a2fcf0c23a65a22191364e0c84f9da34717
Josue Nery <josuepontes2010@hotmail.com>
2022-06-19 16:32:30 -0300

    Add Parser

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from tag import Tag
 from token import Token
 from lexer import Lexer
+from parser import Parser
 
 if __name__ == "__main__":
    lexer = Lexer('HelloWorld.txt')
+   parser = Parser(lexer)
 
-   print("\n=>Lista de tokens:")
-   
-   token = lexer.proxToken()
-   while(token is not None and token.getNome() != Tag.EOF):
-      print(token.toString(), "Linha: " + str(token.getLinha()) + " Coluna: " + str(token.getColuna()))
-      token = lexer.proxToken()
+   parser.Program()
+
+   parser.lexer.closeFile()
 
    print("\n=>Tabela de simbolos:")
    lexer.printTS()
